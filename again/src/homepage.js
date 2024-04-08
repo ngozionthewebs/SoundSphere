932077ea8ed501d6e97bc1d34065132bc102bdea
ngozionthewebs <231285@virtualwindow.co.za>
2024-04-08 13:51:26 +0200

    final touches

--- again/src/homepage.js
+++ again/src/homepage.js
@@ -9,7 +9,7 @@ const Homepage = () => {
 
                 <div className="heading">Welcome to <br/> Sound Sphere*</div>
 
-                <div className="subtext">This web app allows users <br/> to compare music based on <br/> specific properties. <br/> What Genre are you? <br/> lets find out :)   </div>
+                <div className="subtext">This web app allows users <br/> to compare music based on <br/> specific properties. <br/> who did it better <br/> lets find out :)   </div>
             </div>
 
             <div className="dashboard">
